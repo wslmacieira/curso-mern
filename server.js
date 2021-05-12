e1f15d853dac0d994112ad01b77ac80c3d1775f3
wslmacieira <wslmacieira@outlook.com>
2021-05-12 01:00:26 -0300

    Conexão com MongoDB

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 3333;
+
+mongoose.connect('mongodb+srv://admin:root@cluster0.vwjp1.mongodb.net/curso-basico-mearn', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: false
+}, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('MongoDB CONECTADO com sucesso!')
+  }
+});
+
 app.use(cors());
-
 app.use(cookieParser());
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
